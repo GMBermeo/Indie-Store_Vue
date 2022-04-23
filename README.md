88791dc6b79c429b9f1b885d61300d31b09d0e9f
Guilherme Bermêo <gm@bermeo.dev>
2022-04-23 13:06:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fioZero
+ 
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="w-40 h-40 bg-red-500 rounded-full" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
