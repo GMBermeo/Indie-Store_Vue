41f062f5051c7371bad1ee2d3a785653b0b09943
Guilherme Bermêo <gm@bermeo.dev>
2022-05-23 23:24:53 -0300

    🤥 Tentativa de SSR

--- src/main.js
+++ src/main.js
@@ -4,4 +4,16 @@ import "./assets/css/tailwind.css";
 import "./assets/css/fonts.css";
 import "./assets/css/main.css";
 
-createApp(App).mount("#app");
+import { createSSRApp } from "vue";
+
+import { renderToString } from "vue/server-renderer";
+
+const app = createSSRApp({
+  data: () => createApp(App).mount("#app"),
+});
+
+renderToString(
+  app.then((html) => {
+    console.log(html);
+  })
+);
